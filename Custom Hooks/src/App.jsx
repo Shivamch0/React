061052441a9e1d4fe6d41109c322ffb811b590a5
Shivam <shivamch0865@gmail.com>
2026-04-08 11:31:21 +0530

    Custom Hooks use Completed

--- Custom Hooks/src/App.jsx
+++ Custom Hooks/src/App.jsx
@@ -1,11 +1,41 @@
-import "./App.css"
+import "./App.css";
+import { useCart } from "./hooks/useCart.js"
+import  {products} from "./data/Products.js"
+import Product from "./components/Product.jsx"
+import Cart from "./components/Cart.jsx"
+import CartItem from "./components/CartItem.jsx";
 
 function App() {
+
+  const { cart , addToCart , removeFromCart , updateCart , total } = useCart();
  
   return (
     <>
-     <div></div>
-     <h1>Custom Hooks</h1>
+     <div className="app">
+      <header>
+        <h1>Shopping Cart</h1>
+      </header>
+      <main className="products">
+        <section>
+          {
+          products.map(product => (
+            <Product 
+              key={product.id}
+              product={product}
+              onAddToCart={addToCart}
+            />
+          ))
+        }
+        </section>
+        <Cart 
+        cart={cart}
+        onUpdateQuantity={updateCart}
+        onRemove={removeFromCart}
+        total={total}
+        />
+      </main>
+     </div>
+     
     </>
   )
 }
